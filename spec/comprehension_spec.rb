6bf18ca24cd850949be07f1044c517af6110e8b6
Xue(Yuki) Yao <ukiyao@hotmail.com>
2019-07-26 05:34:13 +0000

    Automatically backed up by Learn

--- spec/comprehension_spec.rb
+++ spec/comprehension_spec.rb
@@ -30,7 +30,7 @@ describe "GIT" do
 # describe "PUSHING:" do
   it "5. From the master branch, what is the syntax for pushing the master branch 
       to the remote master branch where the remote is called 'origin'?" do
-    answer = "?"
+    answer = ""
     encoded_answer = "6c8e01a0360da3cf6e314c69c453957b70ab1b4e"
     expect(encode(answer)).to eq(encoded_answer)
   end
